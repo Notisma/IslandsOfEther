c8b5f757bafd4bd32bf6708bb59b7405cde83fbb
fuertesn <noe.fuertes-torredeme@etu.umontpellier.fr>
2024-01-23 13:26:29 +0100

    drag cards

--- Assets/Scripts/CardClick.cs
+++ Assets/Scripts/CardClick.cs
@@ -8,23 +8,38 @@ public class CardClick : MonoBehaviour
 {
     private Color _originalColor;
     private SpriteRenderer _spriteRenderer;
+    public Plane dragPlane;
+    public Vector3 offset;
+    public Camera myMainCamera; 
+
 
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        myMainCamera = Camera.main;
     }
 
     public void OnMouseDown()
     {
         print("Card Clicked");
         EnlargeCard();
-        
+        dragPlane = new Plane(myMainCamera.transform.forward, transform.position); 
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition); 
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist); 
+        offset = transform.position - camRay.GetPoint(planeDist);
     }
 
     public void OnMouseUp()
     {
         print("Card dropped");
         ResetCard();
+    }
+
+    public void OnMouseDrag()
+    {
+        MoveCard();
     }
 
 
@@ -38,5 +53,14 @@ public class CardClick : MonoBehaviour
     {
         transform.localScale = new Vector3(1, 1, 1);
         _spriteRenderer.sortingOrder = 0;
+    }
+
+    void MoveCard()
+    {
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition); 
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist);
+        transform.position = camRay.GetPoint(planeDist) + offset;
     }
 }
