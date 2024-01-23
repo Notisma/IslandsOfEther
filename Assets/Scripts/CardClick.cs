9bb5d37d1d9ebc7b0ba1b58087e44ac8e0534100
fuertesn <noe.fuertes-torredeme@etu.umontpellier.fr>
2024-01-23 09:55:39 +0100

    clic sur carte

---
+++ Assets/Scripts/CardClick.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardClick : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        print("Carte cliquée");
+    }
+}
