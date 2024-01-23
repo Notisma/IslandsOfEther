1fce7432d964e24a659857c927d3ad15d171a929
fuertesn <noe.fuertes-torredeme@etu.umontpellier.fr>
2024-01-23 12:47:35 +0100

    select cards

--- Assets/Scripts/CardClick.cs
+++ Assets/Scripts/CardClick.cs
@@ -2,11 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CardClick : MonoBehaviour
 {
+    private Color _originalColor;
+    private SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void OnMouseDown()
     {
-        print("Carte cliquée");
+        print("Card Clicked");
+        EnlargeCard();
+        
+    }
+
+    public void OnMouseUp()
+    {
+        print("Card dropped");
+        ResetCard();
+    }
+
+
+    void EnlargeCard()
+    {
+        transform.localScale *= 1.5f;
+        _spriteRenderer.sortingOrder = 1;
+    }
+
+    void ResetCard()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+        _spriteRenderer.sortingOrder = 0;
     }
 }
