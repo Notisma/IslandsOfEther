6cf1d2ae160ddfe54ecd4c342b79e4fa2602864c
Rsmaili <smailifarahmedia@gmail.com>
2024-01-24 07:17:20 +0300

    protag

--- Assets/Scripts/PlayerSprit.cs
+++ Assets/Scripts/PlayerSprit.cs
@@ -16,19 +16,47 @@ public class PlayerSprit : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            animator.SetInteger("Speed", 0);
+            animator.SetInteger("SpeedDown", 0);
+            animator.SetInteger("SpeedUp", 1);
+            animator.SetInteger("SpeedRight", 0);
+            animator.SetInteger("SpeedLeft", 0);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            animator.SetInteger("Speed", 1);
+            animator.SetInteger("SpeedDown", 1);
+            animator.SetInteger("SpeedUp", 0);
+            animator.SetInteger("SpeedRight", 0);
+            animator.SetInteger("SpeedLeft", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetInteger("SpeedDown", 0);
+            animator.SetInteger("SpeedUp", 0);
+            animator.SetInteger("SpeedRight", 0);
+            animator.SetInteger("SpeedLeft", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.SetInteger("SpeedDown", 0);
+            animator.SetInteger("SpeedUp", 0);
+            animator.SetInteger("SpeedRight", 1);
+            animator.SetInteger("SpeedLeft", 0);
         }
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
-            animator.SetInteger("Speed", 0);
+            animator.SetInteger("SpeedUp", 0);
         }
         if (Input.GetKeyUp(KeyCode.DownArrow))
         {
-            animator.SetInteger("Speed", 0);
+            animator.SetInteger("SpeedDown", 0);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            animator.SetInteger("SpeedLeft", 0);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            animator.SetInteger("SpeedRight", 0);
         }
     }
 }
